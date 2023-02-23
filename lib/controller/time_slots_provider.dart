791d36efa708a5a2894349dd4c89795830ebffcb
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-24 00:09:37 +0600

    check if slot time is tomorrow

--- lib/controller/time_slots_provider.dart
+++ lib/controller/time_slots_provider.dart
@@ -3,12 +3,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class TimeSlotNotifier extends ChangeNotifier {
   List<DateTime> timeSlotList = [];
+  bool isTomorrow = false;
 
   getTmeSlotList(int duration, int slot) {
+    isTomorrow = false;
+    final now = DateTime.now();
+    final tomorrow = DateTime(now.year, now.month, now.day + 1);
+
     for (int i = 1; i <= slot; i++) {
       timeSlotList.add(
           //increase the time by time duration.
           DateTime.now().add(Duration(minutes: duration)));
+      // check if the date time is tomorrow.
+      if (DateTime.now().add(Duration(minutes: duration)) == tomorrow) {
+        isTomorrow = true;
+        notifyListeners();
+      }
     }
     notifyListeners();
   }
