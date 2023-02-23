2683cdc14dde22ef27a87b6a6055cb6c864e4ba5
MahamudulHasan <syedhasan.cse@gmail.com>
2023-02-23 22:59:29 +0600

    init slot screen

---
+++ lib/view/screens/slot_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SlotScreen extends ConsumerStatefulWidget {
+  const SlotScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SlotScreenState();
+}
+
+class _SlotScreenState extends ConsumerState<SlotScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
